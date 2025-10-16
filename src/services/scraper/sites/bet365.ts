77435af42ccac994d293a065ca4a168c9c0dae3d
James Spencer <jameskcs1254@gmail.com>
2025-10-16 08:42:56 +0100

    show url used in error for placeholder bet365 scraper

--- src/services/scraper/sites/bet365.ts
+++ src/services/scraper/sites/bet365.ts
@@ -2,5 +2,5 @@ import { HorseOddsScraperFunc } from '../../../types/odds.types';
 
 export const scrapeBet365: HorseOddsScraperFunc = async (url) => {
   // Placeholder function for Bet365 scraper
-  throw new Error('Bet365 scraper not implemented yet');
+  throw new Error('Bet365 scraper not implemented yet, url used: ' + url);
 }
